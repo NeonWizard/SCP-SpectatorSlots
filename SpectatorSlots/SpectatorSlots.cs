d23288e521fed1ac1619d71e8bbfb62de7cf0136
NeonWizard <averagewizard13@gmail.com>
2018-12-02 19:08:00 -0700

    Initial commit

---
+++ SpectatorSlots/SpectatorSlots.cs
@@ -0,0 +1,36 @@
+using Smod2;
+using Smod2.Attributes;
+using Smod2.EventHandlers;
+using Smod2.Events;
+using Smod2.Config;
+
+namespace SpectatorSlots
+{
+    [PluginDetails(
+        author = "Spooky",
+        name = "SpectatorSlots",
+        description = "Allows extra players to join in overwatch mode while waiting for players to leave.",
+        id = "xyz.wizardlywonders.SpectatorSlots",
+        version = "1.0",
+        SmodMajor = 3,
+        SmodMinor = 1,
+        SmodRevision = 22
+    )]
+    class SpectatorSlots : Plugin
+    {
+        public override void OnDisable()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void OnEnable()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Register()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
